bd50d682de445e25a98e7e52bed286cc98b25421
shreeyak <ss.shrek7@gmail.com>
2018-09-07 14:00:06 +0000

    added control loop for teensy

--- scripts/vesc_control.py
+++ scripts/vesc_control.py
@@ -3,26 +3,29 @@
 import rospy
 from std_msgs.msg import *
 
+topic_motor_throttle = "/commands/motor/current"
+topic_dist_to_person = "/distance_to_person"
 
 class vesc_control:
     def __init__(self):
-        self.pub = rospy.Publisher(
-            '/commands/motor/current', Float64, queue_size=10)
+        self.pub = rospy.Publisher(topic_motor_throttle,
+                                    Float64, queue_size=10)
 
-        self.sub = rospy.Subscriber("/distance_to_person",
+        self.sub = rospy.Subscriber(topic_dist_to_person,
                                     Float32, self.callback,  queue_size=10)
 
         # variables for the PID
-        self.setpoint = 1.0  # in meters
+        self.setpoint = 0.7  # in meters
         self.kp = 5.0
 
     def callback(self, sub_distance_to_person):
         # PID code
         distance_to_person = sub_distance_to_person.data
+        rospy.loginfo("distance_to_person: %f", distance_to_person)
         motor_throttle = 0.0
 
         # A reading of 0 means that the distance is invalid
-        if distance_to_person != 0:
+        if distance_to_person > 0.7:
             error = distance_to_person - self.setpoint
             motor_throttle = error * self.kp
             rospy.loginfo("Motor Throttle: %f", motor_throttle)
