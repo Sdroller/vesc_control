9350abca63e11f6717be53c67f2fbbd62acc7372
shreeyak <ss.shrek7@gmail.com>
2018-09-07 13:40:12 +0000

    added pid loop for vesc

--- scripts/vesc_control.py
+++ scripts/vesc_control.py
@@ -3,18 +3,44 @@
 import rospy
 from std_msgs.msg import *
 
-def talker():
-    rospy.init_node('vesc_control_node', anonymous=True)
-    pub = rospy.Publisher('/commands/motor/current', Float64, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        values=1.5
-        rospy.loginfo(values)
-        pub.publish(values)
-        rate.sleep()
 
-if __name__ == '__main__':
+class vesc_control:
+    def __init__(self):
+        self.pub = rospy.Publisher(
+            '/commands/motor/current', Float64, queue_size=10)
+
+        self.sub = rospy.Subscriber("/distance_to_person",
+                                    Float32, self.callback,  queue_size=10)
+
+        # variables for the PID
+        self.setpoint = 1.0  # in meters
+        self.kp = 5.0
+
+    def callback(self, sub_distance_to_person):
+        # PID code
+        distance_to_person = sub_distance_to_person.data
+        motor_throttle = 0.0
+
+        # A reading of 0 means that the distance is invalid
+        if distance_to_person != 0:
+            error = distance_to_person - self.setpoint
+            motor_throttle = error * self.kp
+            rospy.loginfo("Motor Throttle: %f", motor_throttle)
+        self.pub.publish(motor_throttle)
+
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    vc = vesc_control()
+    rospy.init_node('vesc_control_node', anonymous=True)
+    rospy.loginfo("Yolo Depth Measure node started")
+
     try:
-        talker()
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
