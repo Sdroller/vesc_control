9a62e699b1ccaeb20bc2386727e3a2e2924c63f9
Colin Smith <cj.colinsmith@gmail.com>
2018-09-14 10:52:47 -0600

    added turning PID and subscriber to centroid_pos_x topic

--- scripts/teensy_control.py
+++ scripts/teensy_control.py
@@ -3,34 +3,90 @@
 import rospy
 from std_msgs.msg import *
 
-topic_motor_throttle = 'set_throttle_L'
+topic_motor_throttle_L = 'set_throttle_L'
+topic_motor_throttle_R = 'set_throttle_R'
+topic_motor_brake_L = 'set_brake_L'
+topic_motor_brake_R = 'set_brake_R'
 topic_dist_to_person = "/distance_to_person"
+topic_position_x = 'centroid_pos_x'
 
+
+
+# ADD ADDITIONAL SUBSCRIBER LOOKING AT CENTROID x LOCATION
+# CALCULATE THE THROTTLE SIGNALS FOR CORRECTION
+# ADD THROTTLE SIGNALS TO SYMMETRIC THROTTLE SIGNALS
+# THIS IS CHALLENGING AND IMPRECISE SINCE WE ARE USING THROTTLE INSTEAD OF WHEEL VELOCITY
 class vesc_control:
     def __init__(self):
-        self.pub = rospy.Publisher(
-            topic_motor_throttle, Int16, queue_size=10)
+        self.pub_TL = rospy.Publisher(
+            topic_motor_throttle_L, Int16, queue_size=10)
 
-        self.sub = rospy.Subscriber(topic_dist_to_person,
-                                    Float32, self.callback,  queue_size=10)
+        self.pub_TR = rospy.Publisher(
+            topic_motor_throttle_R, Int16, queue_size=10)
+        
+        self.pub_BL = rospy.Publisher(
+            topic_motor_brake_L, Int16, queue_size=10)
+                
+        self.pub_BR = rospy.Publisher(
+            topic_motor_brake_R, Int16, queue_size=10)
+
+        self.sub_z = rospy.Subscriber(topic_dist_to_person,
+                                    Float32, self.callback_z,  queue_size=10)
+        
+        self.sub_x = rospy.Subscriber(topic_position_x,
+                                    Float32, self.callback_x,  queue_size=10)
 
         # variables for the PID
         self.setpoint = 0.7  # in meters
-        self.kp = 3000.0
+        self.kp_z = 3000.0 # gain for distance pid
+        self.kp_x = 3000.0 # gain for turning pid
+        self.wheelbase = 36 # inches
+        self.wheel_radius = 8 # inches
+        self.motor_throttle_asym_L = 0.0
+        self.motor_throttle_asym_R = 0.0
+        self.motor_throttle_sym = 0.0
+        self.img_width = 376
 
-    def callback(self, sub_distance_to_person):
+    def callback_z(self, sub_position_z):
+        # subrcriber provides a data and the throttle and brake topics are updated
         # PID code
-        distance_to_person = sub_distance_to_person.data
-        rospy.loginfo("distance_to_person: %f", distance_to_person)
-        motor_throttle = 0.0
+        centroid_pos_z = sub_position_z.data
+        rospy.loginfo("distance_to_person: %f", centroid_pos_z)
+        self.motor_throttle_sym = 0.0
 
         # A reading of 0 means that the distance is invalid
-        if distance_to_person > 0.7:
-            error = distance_to_person - self.setpoint
-            motor_throttle = error * self.kp
-            rospy.loginfo("Motor Throttle: %f", motor_throttle)
-        self.pub.publish(motor_throttle)
+        # calculate the throttle from the PID to keep the distance at the setpoint
+        if centroid_pos_z > 0.7:
+            error = centroid_pos_z - self.setpoint
+            self.motor_throttle_sym = error * self.kp_z
+            rospy.loginfo("Motor Throttle Symmetric: %f", self.motor_throttle_sym)
+        self.publish_throttle()    
+    
+    def callback_x(self, sub_position_x):
+        # subrcriber provides horiz position data and the throttle and brake topics are updated
+        # PID code
+        centroid_pos_x = sub_position_x.data
+        rospy.loginfo("centroid_pos_x: %f", centroid_pos_x)
+        self.motor_throttle_asym_L = 0.0
+        self.motor_throttle_asym_R = 0.0
 
+        # calculate the throttle reading necessary to turn the stroller
+        # this is another basic PID loop tying the deflection angle to the wheel speeds
+        error = centroid_pos_x - self.img_width/2
+        asym = error * self.kp_x
+        self.motor_throttle_asym_L = asym
+        self.motor_throttle_asym_R = -asym
+        rospy.loginfo("Motor Throttle Asymmetric: %f", self.motor_throttle_sym)
+        self.publish_throttle()
+
+    def publish_throttle(self):
+        # publish to the two throttle topics, sum the throttle resonse for turning and distance maintenance.
+        throttle_L = self.motor_throttle_sym + self.motor_throttle_asym_L
+        throttle_R = self.motor_throttle_sym + self.motor_throttle_asym_R
+        rospy.loginfo("Motor Throttle L: %f", throttle_L)
+        rospy.loginfo("Motor Throttle R: %f", throttle_R)        
+        self.pub_TL.publish(throttle_L)
+        self.pub_TR.publish(throttle_R)
 
 def main(args):
     '''Initializes and cleanup ros node'''
